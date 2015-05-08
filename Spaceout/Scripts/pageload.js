644be967ac407a09b6d78ce727d110c61ae1f421
Gunnar Hafdal <g@gunn.si>
2015-05-08 14:11:38 +0200

    fixed popstate

--- Spaceout/Scripts/pageload.js
+++ Spaceout/Scripts/pageload.js
@@ -10,33 +10,15 @@
         $("#body").load(href + " #body", function (data) {
             var $html = $("<div />").append($.parseHTML(data));
             $("head>title").remove();
-            $html.find("title").prependTo("head");
-            history.pushState({}, $("head>title").text(), href);
+            $html.find("title").prependTo("head");   
         });
     }
 
     $("#menu a").click(function (event) {
         event.preventDefault();
         var href = $(this).attr('href');
-        //$.ajax({
-        //    url: href,
-        //    type: 'GET',
-        //    dataType: 'html'
-        //})
-        //.done(function (data, status, xhr) {
-        //    console.log("status: %s", status);
-        //    var $html = $("<div />").append($.parseHTML(data));
-        //    $("head>title").remove();
-        //    $html.find("title").prependTo("head");
-        //    history.pushState({}, $("head>title").text(), href);
-        //    console.log(data);
-        //})
-        //.fail(function() {
-        //    console.log("error");
-        //})
-        //.always(function() {
-        //    console.log("complete");
-        //});
+
         loadContent(href);
+        history.pushState({}, $("head>title").text(), href);
     });
 });
