699c625859b557c38f841dd7d37f60e69eabfe91
Gunnar Hafdal <g@gunn.si>
2015-05-08 12:54:57 +0200

    Added a AJAX call for all the urls in the menu

--- Spaceout/Scripts/pageload.js
+++ Spaceout/Scripts/pageload.js
@@ -2,6 +2,20 @@
     console.log("yes");
 
     $("#menu a").click(function () {
-        
+        var href = $(this).attr('href');
+        $.ajax({
+            url: href,
+            type: 'GET',
+            dataType: 'html'
+        })
+        .done(function() {
+            console.log("success");
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
     });
 });
