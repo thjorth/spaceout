05fbf07e7410205d01025f6481e2091032251932
Gunnar Hafdal <g@gunn.si>
2015-05-08 14:08:41 +0200

    Aedded a popstate function, Seperated the content load into a function of its own

--- Spaceout/Scripts/pageload.js
+++ Spaceout/Scripts/pageload.js
@@ -1,5 +1,19 @@
 ﻿$(document).ready(function () {
     console.log("yes");
+    window.onpopstate = function (event) {
+        $("#loading").show();
+        console.log("pathname: " + location.pathname);
+        loadContent(location.pathname);
+    };
+
+    function loadContent(href) {
+        $("#body").load(href + " #body", function (data) {
+            var $html = $("<div />").append($.parseHTML(data));
+            $("head>title").remove();
+            $html.find("title").prependTo("head");
+            history.pushState({}, $("head>title").text(), href);
+        });
+    }
 
     $("#menu a").click(function (event) {
         event.preventDefault();
@@ -23,13 +37,6 @@
         //.always(function() {
         //    console.log("complete");
         //});
-
-        $("#body").load(href + " #body", function (data) {
-            var $html = $("<div />").append($.parseHTML(data));
-            $("head>title").remove();
-            $html.find("title").prependTo("head");
-            history.pushState({}, $("head>title").text(), href);
-            
-        });
+        loadContent(href);
     });
 });
