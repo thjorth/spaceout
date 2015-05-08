f1d881f224c62ee92c6e9ba59b758040fa5a06b1
Gunnar Hafdal <g@gunn.si>
2015-05-08 15:46:01 +0200

    using native js to fetch the html

--- Spaceout/Scripts/pageload.js
+++ Spaceout/Scripts/pageload.js
@@ -8,21 +8,16 @@
     };
 
     function loadContent(href) {
-        $.ajax({
-            url: href
-        })
-        .done(function(data, status, xhr) {
-            $(".main-content").html(data);
+        var r = new XMLHttpRequest();
+        r.open("GET", href, true);
+        r.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        r.onreadystatechange = function () {
+            if (r.readyState != 4 || r.status != 200) return;
+            $(".main-content").html(r.response);
             console.log("success");
-            console.log(xhr.getAllResponseHeaders());
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
-            $("#loading").hide();
-        });
+            console.log(r.getAllResponseHeaders());
+        };
+        r.send();
         
     }
 
