bd4036058bd862ea00ecb97e052b0a3b1bb28fb2
Gunnar Hafdal <g@gunn.si>
2015-05-08 14:03:42 +0200

    PAgelaod now uses the .load fragment function from jQuery to load the page correctly. Still needs to actiavte the back button though

--- Spaceout/Scripts/pageload.js
+++ Spaceout/Scripts/pageload.js
@@ -4,19 +4,32 @@
     $("#menu a").click(function (event) {
         event.preventDefault();
         var href = $(this).attr('href');
-        $.ajax({
-            url: href,
-            type: 'GET',
-            dataType: 'html'
-        })
-        .done(function (html) {
-            console.log("success", html);
-        })
-        .fail(function() {
-            console.log("error");
-        })
-        .always(function() {
-            console.log("complete");
+        //$.ajax({
+        //    url: href,
+        //    type: 'GET',
+        //    dataType: 'html'
+        //})
+        //.done(function (data, status, xhr) {
+        //    console.log("status: %s", status);
+        //    var $html = $("<div />").append($.parseHTML(data));
+        //    $("head>title").remove();
+        //    $html.find("title").prependTo("head");
+        //    history.pushState({}, $("head>title").text(), href);
+        //    console.log(data);
+        //})
+        //.fail(function() {
+        //    console.log("error");
+        //})
+        //.always(function() {
+        //    console.log("complete");
+        //});
+
+        $("#body").load(href + " #body", function (data) {
+            var $html = $("<div />").append($.parseHTML(data));
+            $("head>title").remove();
+            $html.find("title").prependTo("head");
+            history.pushState({}, $("head>title").text(), href);
+            
         });
     });
 });
