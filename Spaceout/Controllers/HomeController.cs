089af7434b5a1ca58cb9440bb92715cb7c9a74b7
Thomas Hjorth <thomas.hjorth@valtech.dk>
2015-05-08 15:42:22 +0200

    Now returns a json result

--- Spaceout/Controllers/HomeController.cs
+++ Spaceout/Controllers/HomeController.cs
@@ -4,16 +4,18 @@ namespace Spaceout.Controllers
 {
 	public class HomeController : SpaceOutController
 	{
-        public ViewResult Index()
+        public ActionResult Index()
 		{
 			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
-            return View();
+			return ViewOrJson("Index", "", null);
 		}
 
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your app description page.";
+
+			return ViewOrJson("About", "", null);
 
             return View();
 		}
@@ -21,6 +23,8 @@ namespace Spaceout.Controllers
 		public ActionResult Contact()
 		{
 			ViewBag.Message = "Your contact page.";
+
+			return ViewOrJson("Contact", "", null);
 
             return View();
 		}
--- Spaceout/Controllers/SpaceOutController.cs
+++ Spaceout/Controllers/SpaceOutController.cs
@@ -1,16 +1,40 @@
 ﻿using System.Web.Mvc;
 using Spaceout.Infrastructure.Http;
+using Spaceout.Helpers;
 
 namespace Spaceout.Controllers
 {
     public class SpaceOutController : Controller
     {
+		protected ActionResult ViewOrJson(string viewName, string masterName, object model)
+		{
+			if (ControllerContext.RequestContext.HttpContext.Request.IsAjaxRequest())
+			{
+				return Json(new {
+					Body = ControllerExtensions.RenderPartialViewToString(this, viewName, model),
+					Model = model
+				}, JsonRequestBehavior.AllowGet);
+			}
+			else
+			{
+				return base.View(viewName, model);
+			}
+
+			/*
+			return ControllerContext.RequestContext.HttpContext.Request.IsAjaxRequest()
+				? Json(ControllerExtensions.RenderPartialViewToString(this, viewName, model))
+				: base.View(viewName, masterName, model);
+			*/
+		}
+
+		/*
         protected override ViewResult View(string viewName, string masterName, object model)
         {
             //If AJAX request, sent a partial view
             return ControllerContext.RequestContext.HttpContext.Request.IsAjaxRequest()
-                ? CustomPartialViewResult.Convert(PartialView(viewName, model))
+                ? Json(ControllerExtensions.RenderPartialViewToString(this, viewName, model))
                 : base.View(viewName, masterName, model);
         }
+		 * */
     }
 }
